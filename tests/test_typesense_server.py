a9d62f3703b47b6941f8c3d0ac59bf8c44c0a21d
Grant Jenks <grant.jenks@gmail.com>
2023-07-06 15:04:02 +0000

    update run() test

--- tests/test_typesense_server.py
+++ tests/test_typesense_server.py
@@ -1,4 +1,7 @@
 """Test Python wrapper for Typesense Server"""
+import os
+import shutil
+import time
 import typesense_server
 
 
@@ -6,5 +9,9 @@ def test_title():
     assert typesense_server.__title__ == 'python-typesense-server-wrapper'
 
 
-def test_start():
-    typesense_server.start()
+def test_run()):
+    shutil.rmtree('data', ignore_errors=True)
+    os.mkdir('data')
+    args = ['--api-key', 'abc', '--data-dir', 'data']
+    with typesense_server.run(*args) as proc:
+        time.sleep(10)
