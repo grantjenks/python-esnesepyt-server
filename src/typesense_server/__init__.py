0b98192f76970547822f3e0337b0ad17c219b875
Grant Jenks <grant.jenks@gmail.com>
2023-07-06 01:09:20 +0000

    Add simple utils

--- src/typesense_server/__init__.py
+++ src/typesense_server/__init__.py
@@ -1,9 +1,30 @@
 """Typesense Server
 """
 
+import contextlib
+import pathlib
+import subprocess
 
-def start():
-    pass
+
+def get_path():
+    "Get Typesense Server path."
+    init_path = pathlib.Path(__file__)
+    bin_path = init_path.parent / 'typesense-server'
+    return bin_path.resolve()
+
+
+@contextlib.contextmanager
+def run(*typesense_server_args, **proc_args):
+    """Run Typesense Server
+
+    :param list typesense_server_args: list of arguments for Typesense Server
+    :param dict proc_args: dict of subprocess.Popen arguments
+    """
+    bin_path = str(get_path())
+    proc = subprocess.Popen([bin_path, *typesense_server_args], **proc_args)
+    with proc:
+        yield proc
+        proc.terminate()
 
 
 __title__ = 'python-typesense-server-wrapper'
