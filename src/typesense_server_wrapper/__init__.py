ebfd1b776339606b154a8d58e7787c0001ce5d6f
Grant Jenks <grant.jenks@gmail.com>
2023-08-13 16:22:58 -0700

    Bump version to 0.3.0

--- src/typesense_server_wrapper/__init__.py
+++ src/typesense_server_wrapper/__init__.py
@@ -47,7 +47,7 @@ def run(*typesense_server_args, **proc_args):
 
 
 __title__ = 'typesense-server-wrapper'
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = '2023, Grant Jenks'
