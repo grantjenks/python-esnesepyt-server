fa31056b87b911b7fb5e87c4f480b705a1e6a7b8
Grant Jenks <grant.jenks@gmail.com>
2023-08-13 16:22:25 -0700

    Assemble the bin path

--- src/typesense_server_wrapper/__init__.py
+++ src/typesense_server_wrapper/__init__.py
@@ -3,14 +3,33 @@
 
 import contextlib
 import pathlib
+import shutil
+import stat
 import subprocess
+
+import typesense_server_wrapper_alpha as ts_alpha
+import typesense_server_wrapper_omega as ts_omega
 
 
 def get_path():
     """Get Typesense Server path."""
     init_path = pathlib.Path(__file__)
-    bin_path = init_path.parent / 'typesense-server'
-    return bin_path.resolve()
+    bin_path = (init_path.parent / 'typesense-server').resolve()
+
+    if not bin_path.exists():
+        alpha_path = ts_alpha.get_path()
+
+        with alpha_path.open('rb') as reader, bin_path.open('wb') as writer:
+            shutil.copyfileobj(reader, writer)
+
+        omega_path = ts_omega.get_path()
+
+        with omega_path.open('rb') as reader, bin_path.open('ab') as writer:
+            shutil.copyfileobj(reader, writer)
+
+        bin_path.chmod(bin_path.stat().st_mode | stat.S_IEXEC)
+
+    return bin_path
 
 
 @contextlib.contextmanager
